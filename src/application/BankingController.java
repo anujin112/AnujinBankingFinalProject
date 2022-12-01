5244634410ba17bfcdbd7616fcb32e0dc005e12e
AnujinMunkhnasan <anujin.munkhnasan@ucalgary.ca>
2022-12-01 21:48:30 +0000

    Attempted to implement first functioning addFunds method.

--- src/application/BankingController.java
+++ src/application/BankingController.java
@@ -1,6 +1,7 @@
 package application;
 
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
@@ -15,28 +16,67 @@ public class BankingController {
 	
     @FXML
     private TextField fundsTextfield;
-
+    
+    @FXML
+    private TextField toAdd;
+    
+    @FXML
+    private Label availFunds;
+    
+    @FXML
+    private double funds;
+    
     @FXML
     void mainMenu(ActionEvent event) { // first "Next" button
     	VBox mainMenuContainer = new VBox();
+    	VBox addFundsContainer = new VBox();
+    	VBox subtractFundsContainer = new VBox();
     	Scene mainMenuScene = new Scene(mainMenuContainer);
-    	double funds = Double.parseDouble(fundsTextfield.getText());
+    	Scene addFundsScene = new Scene(addFundsContainer);
+    	Scene subtractFundsScene = new Scene(subtractFundsContainer);
+    	funds = Double.parseDouble(fundsTextfield.getText());
     	
+    	// main menu widgets
     	Label mainMenuTitle = new Label("Main Menu");
-    	Label availFunds = new Label("Available funds: $" + fundsTextfield.getText());
+    	availFunds.setText("Available funds: $" + funds);
     	Button addFundsButton = new Button("Add to funds");
+    	addFundsButton.setOnAction(e -> applicationStage.setScene(addFundsScene));
     	Button subtractFundsButton = new Button("Subtract from funds");
+    	subtractFundsButton.setOnAction(e -> applicationStage.setScene(subtractFundsScene));
     	Button expensesButton = new Button("Calculate monthly expenses");
     	Button exitButton = new Button("Exit");
-    	mainMenuContainer.getChildren().addAll(mainMenuTitle, availFunds, addFundsButton, subtractFundsButton, expensesButton, exitButton);
+    	mainMenuContainer.getChildren().addAll(mainMenuTitle, addFundsButton, subtractFundsButton, expensesButton, exitButton);
     	applicationStage.setScene(mainMenuScene);
     	
-    	if (funds > 0) {
+    	// add funds widgets
+    	Label addFundsTitle = new Label("Add Funds");
+    	TextField toAdd = new TextField();
+    	Button addButton = new Button("Add");
+    	addButton.setOnAction(addEvent -> addFunds(mainMenuScene, toAdd));
+    	addFundsContainer.getChildren().addAll(addFundsTitle, toAdd, addButton);
+    	
+    	// subtract funds widgets
+    	Label subtractFundsTitle = new Label("Subtract Funds");
+    	TextField toSub = new TextField();
+    	Button subButton = new Button("Subtract");
+    	subtractFundsContainer.getChildren().addAll(subtractFundsTitle, toSub, subButton);
+    	
+    	// first draft error handling
+    	/*if (funds > 0) {
     		System.out.println("Current funds: " + funds);
     	} else if (funds == 0) {
     		System.out.println("Please enter an amount");
     	} else {
     		System.out.println("Invalid input");
-    	}
+    	}*/
     }
+
+    void addFunds(Scene mainMenuScene, TextField toAdd) {
+    	applicationStage.setScene(mainMenuScene);
+    	double startingFunds = Double.parseDouble(fundsTextfield.getText());
+    	double toAddDouble = Double.parseDouble(toAdd.getText());
+    	
+    	funds = startingFunds + toAddDouble;
+    }
+    
 }
